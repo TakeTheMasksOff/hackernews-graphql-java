61d4a4b42bf5290bc32c0662560d0728dcef223f
Khalafbayli Ahmad <halafbeylia@gmail.com>
2019-01-22 23:44:59 +0400

    added first Query

--- src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -2,13 +2,19 @@ package com.howtographql.hackernews;
 
 import com.coxautodev.graphql.tools.SchemaParser;
 import javax.servlet.annotation.WebServlet;
+import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
     public GraphQLEndpoint() {
-        super(SchemaParser.newParser().file("schema.graphqls") // parse the schema file created earlier
-                .build().makeExecutableSchema());
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser().file("schema.graphqls").resolvers(new Query(linkRepository)).build()
+                .makeExecutableSchema();
     }
 }
